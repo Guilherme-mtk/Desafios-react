7afba54fca2ba48bfa04d6ef7cb1426f2cf188ea
Guilherme-mtk <guilhermejesuslima32@gmail.com>
2021-05-27 21:10:51 -0300

    Desafio react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-
-const Titulo = ({ cor, texto }) => {
-  return <h1 style={{ color: cor }}>{texto}</h1>;
-};
+import Header from './Header';
+import Home from './Home';
+import Produtos from './Produtos';
 
 const App = () => {
+  const { pathname } = window.location;
+
+  let Component;
+  if (pathname === '/produtos') {
+    Component = Produtos;
+  } else {
+    Component = Home;
+  }
+
   return (
-    <div>
-      <Titulo cor="red" texto="Meu titulo 1" />
-      <Titulo cor="blue" texto="Meu titulo 2" />
-    </div>
+    <section>
+      <Header />
+      <Component />
+    </section>
   );
 };
 
