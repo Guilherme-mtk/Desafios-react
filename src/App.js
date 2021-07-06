d16d046bcea42fda5dbc55009606cf5c828cd5d7
Guilherme-mtk <guilhermejesuslima32@gmail.com>
2021-07-06 18:31:26 -0300

    realizado exercicio de formulario de perguntas

--- src/App.js
+++ src/App.js
@@ -1,50 +1,87 @@
 import React from 'react';
-import Input from './Form/Input';
-import Select from './Form/Select';
 import Radio from './Form/Radio';
-import Checkbox from './Form/Checkbox';
+
+const perguntas = [
+  {
+    pergunta: 'Qual método é utilizado para criar componentes?',
+    options: [
+      'React.makeComponent()',
+      'React.createComponent()',
+      'React.createElement()',
+    ],
+    resposta: 'React.createElement()',
+    id: 'p1',
+  },
+  {
+    pergunta: 'Como importamos um componente externo?',
+    options: [
+      'import Component from "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    resposta: 'import Component from "./Component"',
+    id: 'p2',
+  },
+  {
+    pergunta: 'Qual hook não é nativo?',
+    options: ['useEffect()', 'useFetch()', 'useCallback()'],
+    resposta: 'useFetch()',
+    id: 'p3',
+  },
+  {
+    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
+    options: ['set', 'get', 'use'],
+    resposta: 'use',
+    id: 'p4',
+  },
+];
 
 const App = () => {
-  const [nome, setNome] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [produto, setProduto] = React.useState('');
-  const [cor, setCor] = React.useState('Vermelho');
-  const [frutas, setFrutas] = React.useState('');
-  const [linguagens, setLinguagens] = React.useState([]);
-  const [termos, setTermos] = React.useState([]);
+  const [respostas, setRespostas] = React.useState({
+    p1: '',
+    p2: '',
+    p3: '',
+    p4: '',
+  });
+  const [slide, setSlide] = React.useState(0);
+  const [resultado, setResultado] = React.useState(null);
+
+  function handleChange({ target }) {
+    setRespostas({ ...respostas, [target.id]: target.value });
+  }
+
+  function resultadoFinal() {
+    const corretas = perguntas.filter(
+      ({ id, resposta }) => respostas[id] === resposta,
+    );
+    setResultado(`Você acertou: ${corretas.length} de ${perguntas.length}`);
+  }
+
+  function handleClick() {
+    if (slide < perguntas.length - 1) {
+      setSlide(slide + 1);
+    } else {
+      setSlide(slide + 1);
+      resultadoFinal();
+    }
+  }
+
   return (
-    <form>
-      <h2>Termos</h2>
-      <Checkbox
-        options={['Li e aceito os termos.']}
-        value={termos}
-        setValue={setTermos}
-      />
-
-      <h2>Checkbox</h2>
-      <Checkbox
-        options={['JavaScript', 'PHP', 'Ruby']}
-        value={linguagens}
-        setValue={setLinguagens}
-      />
-
-      <h2>Cores</h2>
-      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
-      <h2>Frutas</h2>
-      <Radio
-        options={['Limão', 'Laranja', 'Uva']}
-        value={frutas}
-        setValue={setFrutas}
-      />
-
-      <Select
-        options={['Smartphone', 'Tablet']}
-        value={produto}
-        setValue={setProduto}
-      />
-      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
-      <Input id="email" label="Email" value={email} setValue={setEmail} />
-      <button>Enviar</button>
+    <form onSubmit={(event) => event.preventDefault()}>
+      {perguntas.map((pergunta, index) => (
+        <Radio
+          active={slide === index}
+          key={pergunta.id}
+          value={respostas[pergunta.id]}
+          onChange={handleChange}
+          {...pergunta}
+        />
+      ))}
+      {resultado ? (
+        <p>{resultado}</p>
+      ) : (
+        <button onClick={handleClick}>Próxima</button>
+      )}
     </form>
   );
 };
