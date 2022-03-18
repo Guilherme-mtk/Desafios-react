b0f902f2fb730651dbbaebaa62d7a865434f2311
Guilherme-mtk <guilhermejesuslima32@gmail.com>
2022-03-18 18:40:36 -0300

    exercicio react todolist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
+import { useState } from 'react';
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Footer from './Comoponents/Footer';
-import Header from './Comoponents/Header';
-import Produtos from './Comoponents/Produtos';
-import Contato from './Comoponents/Contato';
-import Produto from './Comoponents/Produto';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [currentText, setCurrentText] = useState('');
+  const [todoId, setTodoId] = useState('');
+
+  function handleClick() {
+    if (!todoId) {
+      setTodos([
+        { id: Math.floor(Math.random() * 100), text: currentText },
+        ...todos,
+      ]);
+    }
+    if (todoId) {
+      const usersEdite = todos.map((todo) => {
+        return todo.id === todoId ? { ...todo, text: currentText } : todo;
+      });
+      setTodos(usersEdite);
+      setTodoId('');
+    }
+    setCurrentText('');
+  }
+
+  const handleDelete = ({ id }) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  function handleEdit(todo) {
+    setTodoId(todo.id);
+    setCurrentText(todo.text);
+  }
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Produtos />} />
-            <Route path="produto/:id" element={<Produto />} />
-            <Route path="contato" element={<Contato />} />
-          </Routes>
+    <div className="container">
+      <div className="list-container">
+        <div className="div-input">
+          <input
+            value={currentText}
+            onChange={(e) => setCurrentText(e.target.value)}
+            type="text"
+          />
+          <button className="btn" onClick={handleClick}>
+            {!todoId ? 'Add' : 'Save'}
+          </button>
         </div>
-        <Footer />
-      </BrowserRouter>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}
+              <button onClick={() => handleDelete(todo)}>Remove</button>
+              <button onClick={() => handleEdit(todo)}>Edite</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
